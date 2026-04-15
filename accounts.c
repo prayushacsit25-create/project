a27555803814bd8df57a81ff605a262a03ff190f
prayushacsit25 <prayushacsit25@oic.edu.np>
2026-04-15 21:29:35 +0545

    first commiit

--- accounts.c
+++ accounts.c
@@ -6,13 +6,15 @@ struct bank{
     float balance;
 };
 void main(){
-    struct bank b[5];
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    struct bank b[n];
     int i;
     FILE *fp;
-    fp=fopen("bank.txt","w");
-    printf("Success\n");
-    for(i=0;i<5;i++){
-        printf("Enter details of %d student",i+1);
+    fp=fopen("record.txt","w");
+    for(i=0;i<n;i++){
+        printf("Enter details of %d person",i+1);
         printf("\nAccount number=");
         scanf("%d",&b[i].acc_no);
          printf("\nName=");
