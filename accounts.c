4200899e7db18ec31caf32c6d26bfea078e85f6a
prayushacsit25 <prayushacsit25@oic.edu.np>
2026-04-28 08:11:02 +0545

    Added my file

--- accounts.c
+++ accounts.c
@@ -1,30 +1,34 @@
 #include<stdio.h>
-struct bank{
+struct account{
     int acc_no;
     char name[20];
-    char address[50];
-    float balance;
+    char address[20];
+    int balance;
+    int time;
 };
 void main(){
-    int n;
-    printf("Enter the value of n:");
-    scanf("%d",&n);
-    struct bank b[n];
+    struct bank a[4];
     int i;
     FILE *fp;
-    fp=fopen("record.txt","w");
-    for(i=0;i<n;i++){
+    fp=fopen("bank.txt","w");
+    printf("Enter details of 4 customers:")
+    for(i=0;i<4;i++){
         printf("Enter details of %d person",i+1);
         printf("\nAccount number=");
-        scanf("%d",&b[i].acc_no);
+        scanf("%d",&a[i].acc_no);
          printf("\nName=");
-        scanf("%s",b[i].name);
+        scanf("%s",a[i].name);
          printf("\nAddress=");
-        scanf("%s",b[i].address);
+        scanf("%s",a[i].address);
          printf("\nBalance=");
-        scanf("%f",&b[i].balance);
-        fprintf(fp,"%d %s %s %f\n",b[i].acc_no,b[i].name,b[i].address,b[i].balance);
+        scanf("%d",&a[i].balance);
+        printf("\nRecent transaction month:");
+        scanf("%d",&a[i].time);
+        fprintf(fp,"%d %s %s %d %d\n",a[i].acc_no,a[i].name,a[i].address,a[i].balance,a[i].time);
     }
     printf("Successful");
     fclose(fp);
+
 }
+
+
